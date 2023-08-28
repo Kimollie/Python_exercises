5b1e54e8f4e9cea96c9c9cadc61a8e5d51054635
Kim T <sylviakimtran@gmail.com>
2023-08-28 21:21:49 +0300

    Ex 2.1 - 2.4 completed

--- Exercise 1 & 2.py
+++ Exercise 1 & 2.py
@@ -1,3 +1,44 @@
-name = input("What's your name?")
+import math
+#Ex1 and 2.1
+"""name = input("What's your name?")
 print("Hello, "+name)
-print("Pull trial")
+
+#Ex 2.2
+radius = input("Enter radius: ")
+area = float(math.pi) * float(radius) * float(radius)
+print(f"Area of this circle is: {area: .2f}")
+
+#Ex 2.3
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+perimeter = (length+width)*2
+rec_area = length * width
+print(f"Perimeter of this rectangle: {perimeter: .2f}")
+print(f"Area of this rectangle: {rec_area: .2f}")
+
+#Ex 2.4
+number1 = int(input("Enter first integer number:"))
+number2 = int(input("Enter second integer number:"))
+number3 = int(input("Enter third integer number:"))
+sum = number1 + number2 + number3
+product = number1*number2*number3
+average = sum/3
+print(f"Sum: {sum}")
+print(f"Product: {product}")
+print(f"Average: {average}")
+"""
+#Ex 2.5
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+
+
+
+
+
+
+
+
+
+
+
