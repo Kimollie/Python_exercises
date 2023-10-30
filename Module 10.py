bab86a1b057896ee470a0cb9eb485d0d6fbcb03e
Kim T <sylviakimtran@gmail.com>
2023-10-30 17:39:16 +0200

    Module 10 edited

--- Module 10.py
+++ Module 10.py
@@ -12,6 +12,7 @@ class Elevator:
                 self.go_up()
             elif self.current_floor > floor:
                 self.go_down()
+            print(self.current_floor)
         return self.current_floor
 
     def go_up(self):
@@ -20,7 +21,6 @@ class Elevator:
     def go_down(self):
         self.current_floor -= 1
 
-
 class Building:
     elevator_list = []
 
@@ -28,28 +28,24 @@ class Building:
         self.top = top
         self.bottom = bottom
         self.elevator_num = elevator_num
-        for h in range(0, elevator_num):
-            h = Elevator(self.top, self.bottom)
+        for h in range(1, elevator_num+1):
+            h = Elevator(self.bottom, self.top)
             self.elevator_list.append(h)
 
-    def run_elevator(self, des_floor):
-        for h in self.elevator_list:
-            h.go_to_floor(des_floor)
+    def run_elevator(self,elevator_index, des_floor):
+        self.elevator_list[elevator_index].go_to_floor(des_floor)
 
     def fire_alarm(self):
         for h in self.elevator_list:
             h.go_to_floor(self.bottom)
-
+            print(h.current_floor)
 
 # Test elevator class
 elevator = Elevator(1, 7)
 elevator.go_to_floor(5)
-print(elevator.current_floor)
 elevator.go_to_floor(1)
-print(elevator.current_floor)
 
 building1 = Building(1, 8, 3)
-building1.run_elevator(5)
+building1.run_elevator(1,5)
 building1.fire_alarm()
-for ele in building1.elevator_list:
-    print(ele.current_floor)
+
