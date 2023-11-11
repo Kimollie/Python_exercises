767fc2860227ed5ea9ea917179b48623d99e99c3
Kim T <sylviakimtran@gmail.com>
2023-11-11 21:52:56 +0200

    Module 12 completed

--- Module 12.py
+++ Module 12.py
@@ -10,7 +10,7 @@ print(response["value"])
 # Ex 12.2
 city = input("Enter city: ")
 city = city.capitalize()
-key = "769757f0d20573d038a9c08bd4ef1b49"
+key = "########################"
 request = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric"
 response = requests.get(request).json()
 weather = response["weather"][0]["description"]
